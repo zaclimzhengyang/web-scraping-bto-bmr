4757431b56bbcff97ae90c99c24007ba84b47066
limzhengyang <zhengyanglim057@gmail.com>
2022-09-17 15:16:24 +0800

    fix(add-dropdown): get selection

---
+++ old_test-selenium.py
@@ -0,0 +1,115 @@
+from typing import List
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import getpass
+
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def loginUser():
+    # Open your browser, and point it to the login page
+    someVariable = getpass.getpass("Press Enter after You are done logging in") #< THIS IS THE SECOND PART
+    #Here is where you put the rest of the code you want to execute
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://homes.hdb.gov.sg/home/landing')
+# driver.maximize_window()
+
+# log in from landing page
+# driver.maximize_window()
+# time.sleep(2)
+driver.find_element(By.CLASS_NAME, "account-text").click() # click on log in button
+loginUser()
+# element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "primary-nav-link")))
+
+# time.sleep(1)
+# driver.find_element(By.CLASS_NAME, "ndi-redirect-link-text").click() # use password instead
+# time.sleep(1)
+# driver.find_element(By.ID, "SpQrToggle-1FATab").click() # password log in
+
+# manually log in
+# time.sleep(20)
+
+# after logging in
+# component_list = driver.find_element(By.CLASS_NAME, "primary-nav")
+# for element in component_list:
+#     print(element.text)
+driver.implicitly_wait(5)
+# find_element > return 1 element
+# find_elements > return list of elements
+nav_list: WebElement = driver.find_element(By.CLASS_NAME, "primary-nav") # parent component
+# print(f"nav_list: {nav_list.text}, {nav_list.tag_name}")
+unordered_list: WebElement = nav_list.find_element(By.TAG_NAME, "ul") # children component
+li_items: List[WebElement] = unordered_list.find_elements(By.TAG_NAME, "li") # returns a list of children component
+# print(f"unordered_list: {unordered_list.text}, {unordered_list.tag_name}")
+for element in li_items:
+    if element.text == "My Profile":
+        driver.implicitly_wait(5)
+        clickable_element = element.find_element(By.TAG_NAME, "a")
+        clickable_element.click()
+        break
+driver.implicitly_wait(5)
+bto_projects = driver.find_element(By.TAG_NAME, "app-your-flat-application") # parent component consists of 2 childrent
+driver.implicitly_wait(5)
+projects = bto_projects.find_elements(By.TAG_NAME, "app-selection-flat-cards") # children component in list form
+driver.implicitly_wait(5)
+for project in projects: # loop through the children component
+    # check project.text against name of project
+    if project.text == """BTO
+Bukit Merah (May 2022)
+Applied flat type:
+4-Room""":
+        clickable_element = project.find_element(By.TAG_NAME, "a")
+        clickable_element.click()
+
+bmr_project = driver.find_element(By.CLASS_NAME, "card-body")
+# print(f"bmr_project: {bmr_project.text}, {bmr_project.tag_name}")
+bmr_project.click()
+
+
+# select_element = driver.find_element(By.CLASS_NAME, "form-row")
+# options = select_element.find_elements(By.TAG_NAME, "div")
+#
+# for option in options:
+#     print(option.text)
+
+# grid = driver.find_element(By.CLASS_NAME, "mt-5") # able to see number of 3 room and 4 room flats available
+# grid_options = grid.find_elements(By.)
+# print(grid.text)
+# select_element.select_by_value('0')
+
+# form_rows = driver.find_element(By.CLASS_NAME, "row mb-5")
+# print(form_rows.text)
+# select = driver.find_elements(By.TAG_NAME, "select")
+#
+# for opt in select:
+#     # for example
+#     print(opt.text)
+#     opt.click()
+# print(f"form_rows: {form_rows.text}, {form_rows.tag_name}")
+# for index, form_row in enumerate(form_rows):
+#     # if "Choose Ethnic Type" in form_row.text:
+#     print(f"index: {index}, form_row.text: {form_row.text}")
+# print(row_mb_5.text)
+# ethnic_type = row_mb_5.find_element(By.CLASS_NAME, "col-lg-4 col-md-6")
+# print(ethnic_type.text)
+# selects = row_mb_5.find_elements(By.TAG_NAME, "select")
+#
+# for index, select in enumerate(selects):
+#     print(f"index: {index}, select.text: {select.text}")
+
+# driver.find_element(By.TAG_NAME, "app-selection-flat-cards").click()
+# driver.find_element(By.CLASS_NAME, "primary-nav-item js-primary-nav").click()
+# driver.find_element(By.CLASS_NAME, "primary-nav-link").click()
+# driver.find_element(By.NAME, "My Profile").click()
+
+while True:
+    pass
--- test-selenium.py
+++ test-selenium.py
@@ -1,94 +1,103 @@
+from enum import Enum
 from typing import List
-
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-import time
 import getpass
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 
-def loginUser():
+
+def login_user() -> None:
     # Open your browser, and point it to the login page
-    someVariable = getpass.getpass("Press Enter after You are done logging in") #< THIS IS THE SECOND PART
-    #Here is where you put the rest of the code you want to execute
+    getpass.getpass("Press Enter after You are done logging in") #< THIS IS THE SECOND PART
+
+
+def navigate_to_my_profile(driver: webdriver.Chrome) -> None:
+    """
+    Gets the navigation bar, iterate through it to get "My Profile", click on it
+    """
+    outer_nav_element: WebElement = driver.find_element(By.CLASS_NAME, "primary-nav")
+    nav_unordered_list: WebElement = outer_nav_element.find_element(By.TAG_NAME, "ul")
+    nav_selections: List[WebElement] = nav_unordered_list.find_elements(By.TAG_NAME, "li")
+    for nav_selection in nav_selections:
+        if nav_selection.text == "My Profile":
+            clickable_nav = nav_selection.find_element(By.TAG_NAME, "a")
+            clickable_nav.click()
+            break
+
+
+def navigate_to_bukit_merah_ridge(driver: webdriver.Chrome) -> None:
+    """
+    Navigate from "My Profile" -> "Bukit Merah (May 2022)" to "Bukit Merah Ridge (May 2022)"
+    """
+    bto_projects: WebElement = driver.find_element(By.TAG_NAME,
+                                       "app-your-flat-application")  # parent component consists of 2 childrent
+    projects = bto_projects.find_elements(By.TAG_NAME, "app-selection-flat-cards")  # children component in list form
+    for project in projects:  # loop through the children component
+        # check project.text against name of project
+        if "Bukit Merah (May 2022)" in project.text:
+            clickable_element = project.find_element(By.TAG_NAME, "a")
+            clickable_element.click()
+    bmr_project = driver.find_element(By.CLASS_NAME, "card-body")
+    bmr_project.click()
+
+
+class SelectionType(str, Enum):
+    flat_type: str = "Choose Flat Type"
+    ethnic_type: str = "Choose Ethnic Type"
+    block_type: str = "Choose Block No."
+    unknown: str = "Unknown"
+
+    @classmethod
+    def _missing_(cls, value):
+        return cls.unknown
+
+
+def get_selection_type(selection: WebElement) -> SelectionType:
+    option_element = selection.find_element(By.TAG_NAME, "option")
+    option_element_text: str = option_element.text
+    return SelectionType(option_element_text)
+
+
+def select_drop_downs(driver: webdriver.Chrome) -> None:
+    """
+    Selects Dropdowns
+    - Choose Ethnic Type to "Chinese"
+    - Choose Block No. to "Blk 132A"
+    """
+    time.sleep(3)
+    drop_down_parent_container: WebElement = driver.find_element(By.CSS_SELECTOR, "div[class='container my-10']")
+    drop_down_form_row: WebElement = drop_down_parent_container.find_element(By.CSS_SELECTOR, "div[class='form-row']")
+    select_divs: List[WebElement] = drop_down_form_row.find_elements(By.CSS_SELECTOR, "div[class='col-lg-4 col-md-6 ']")
+
+    for index, selection in enumerate(select_divs):
+        print(f"select: {selection.text}")
+        selection_type: SelectionType = get_selection_type(selection)
+        if selection_type == SelectionType.ethnic_type:
+            select_element: WebElement = selection.find_element(By.CSS_SELECTOR, "select[class*='form-control select-secondary']")
+            select_object: Select = Select(select_element)
+            select_object.select_by_visible_text('Chinese')
+        elif selection_type == SelectionType.block_type:
+            select_element: WebElement = selection.find_element(By.CSS_SELECTOR, "select[class*='form-control select-secondary']")
+            select_object: Select = Select(select_element)
+            select_object.select_by_visible_text('Blk 132A')
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://homes.hdb.gov.sg/home/landing')
+driver.implicitly_wait(5) # seconds
 driver.maximize_window()
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
-# log in from landing page
-# driver.maximize_window()
-# time.sleep(2)
+# Go to HDB Landing Page
+driver.get('https://homes.hdb.gov.sg/home/landing')
+
 driver.find_element(By.CLASS_NAME, "account-text").click() # click on log in button
-loginUser()
-# element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "primary-nav-link")))
-
-# time.sleep(1)
-# driver.find_element(By.CLASS_NAME, "ndi-redirect-link-text").click() # use password instead
-# time.sleep(1)
-# driver.find_element(By.ID, "SpQrToggle-1FATab").click() # password log in
-
-# manually log in
-# time.sleep(20)
-
-# after logging in
-# component_list = driver.find_element(By.CLASS_NAME, "primary-nav")
-# for element in component_list:
-#     print(element.text)
-driver.implicitly_wait(5)
-# find_element > return 1 element
-# find_elements > return list of elements
-nav_list: WebElement = driver.find_element(By.CLASS_NAME, "primary-nav") # parent component
-print(f"nav_list: {nav_list.text}, {nav_list.tag_name}")
-unordered_list: WebElement = nav_list.find_element(By.TAG_NAME, "ul") # children component
-li_items: List[WebElement] = unordered_list.find_elements(By.TAG_NAME, "li") # returns a list of children component
-print(f"unordered_list: {unordered_list.text}, {unordered_list.tag_name}")
-for element in li_items:
-    if element.text == "My Profile":
-        driver.implicitly_wait(5)
-        clickable_element = element.find_element(By.TAG_NAME, "a")
-        clickable_element.click()
-        break
-driver.implicitly_wait(5)
-bto_projects = driver.find_element(By.TAG_NAME, "app-your-flat-application") # parent component consists of 2 childrent
-driver.implicitly_wait(5)
-projects = bto_projects.find_elements(By.TAG_NAME, "app-selection-flat-cards") # children component in list form
-driver.implicitly_wait(5)
-for project in projects: # loop through the children component
-    # check project.text against name of project
-    if project.text == """BTO
-Bukit Merah (May 2022)
-Applied flat type:
-4-Room""":
-        clickable_element = project.find_element(By.TAG_NAME, "a")
-        clickable_element.click()
-
-bmr_project = driver.find_element(By.CLASS_NAME, "card-body")
-# print(f"bmr_project: {bmr_project.text}, {bmr_project.tag_name}")
-bmr_project.click()
-
-form_rows = driver.find_elements(By.TAG_NAME, "select")
-
-for index, form_row in enumerate(form_rows):
-    # if "Choose Ethnic Type" in form_row.text:
-    print(f"index: {index}, form_row.text: {form_row.text}")
-# print(row_mb_5.text)
-# ethnic_type = row_mb_5.find_element(By.CLASS_NAME, "col-lg-4 col-md-6")
-# print(ethnic_type.text)
-# selects = row_mb_5.find_elements(By.TAG_NAME, "select")
-#
-# for index, select in enumerate(selects):
-#     print(f"index: {index}, select.text: {select.text}")
-
-# driver.find_element(By.TAG_NAME, "app-selection-flat-cards").click()
-# driver.find_element(By.CLASS_NAME, "primary-nav-item js-primary-nav").click()
-# driver.find_element(By.CLASS_NAME, "primary-nav-link").click()
-# driver.find_element(By.NAME, "My Profile").click()
-
-while True:
-    pass
+login_user()
+navigate_to_my_profile(driver=driver)
+navigate_to_bukit_merah_ridge(driver=driver)
+select_drop_downs(driver=driver)
